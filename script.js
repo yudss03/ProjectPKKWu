38992fc30a1db6af366bc5e50af4698d1f4a7a99
Yudhistira <yudhistirawsb@gmail.com>
2024-02-21 20:51:17 +0700

    UPDATEs - 21/2/2024

---
+++ script.js
@@ -0,0 +1,18 @@
+// NAVBAR RESPONSIVE
+
+const menuToggle = document.querySelector('.navbar-burger i');
+const nav = document.querySelector('.navbar-menu');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+
+// FAQ
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", ()=> {
+        faq.classList.toggle('active');
+    })
+})
