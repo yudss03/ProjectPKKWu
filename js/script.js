affcbdf68b54bd0ba69220360ae1d521580387e7
Yudhistira <yudhistirawsb@gmail.com>
2024-03-12 15:22:55 +0700

    UPDATE 12/03/2024

--- js/script.js
+++ js/script.js
@@ -11,9 +11,32 @@ menuToggle.addEventListener('click', function() {
 
 const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq => {
+faqs.forEach((faq) => {
     faq.addEventListener("click", ()=> {
         faq.classList.toggle('active');
     })
 });
 
+// MODAL BOX ACTION
+const listBtns = document.querySelectorAll('.lists .list')
+const modalBox = document.querySelector('.modal')
+const closeBtn = document.querySelector('.modal-container a')
+const modalContent = document.querySelector('.modal-container')
+
+listBtns.forEach((listBtn) => {
+    listBtn.addEventListener('click', () => {
+        modalBox.style.display = 'flex';
+    })
+})
+
+closeBtn.addEventListener('click', (e) => {
+    modalBox.style.display = 'none'
+    e.preventDefault();
+})
+
+modalBox.addEventListener('click', (e) => {
+    if(!modalContent.contains(e.target) && !closeBtn.contains(e.target)) {
+        modalBox.style.display = 'none'
+        e.preventDefault();
+    }
+})
